63c49a3bfcaf12297c43034f310ef652b11bbdcf
Diego Borjas <diegoborjas@gmail.com>
2020-03-16 18:10:58 -0500

    Completed assignment

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,33 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    private String location;
 
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJob() {
+        return jobs;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,36 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @ManyToMany(mappedBy="skills")
+    private final List<Job> jobs = new ArrayList<>();
+
+    @Size(max = 365)
+    private String description;
+
+    public Skill() {}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void addJob(Job job) {
+        this.jobs.add(job);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
