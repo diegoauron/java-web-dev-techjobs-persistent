da0b22ef4af2b85fafb0d7dc713f70b5478daf8e
Diego Borjas <diegoborjas@gmail.com>
2020-03-16 18:30:18 -0500

    Completed assignment (UPDATED LAST SQL)

--- queries.sql
+++ queries.sql
@@ -15,3 +15,8 @@ WHERE location = 'St Louis City'
 DROP TABLE job
 
 ## Part 4: Test it with SQL
+
+SELECT name, description
+FROM skill LEFT JOIN job_skills
+ON skill.id = job_skills.skills_id
+WHERE skills_id is not null
